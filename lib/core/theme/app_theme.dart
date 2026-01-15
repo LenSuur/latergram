100e9c3e6ba7b7c43fd1133b9a001d7690259c36
LenSuur <lesuur@taltech.ee>
2026-01-15 21:21:13 +0200

    remove light theme

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -26,10 +26,7 @@ class AppTheme {
         elevation: 0,
       ),
 
-      cardTheme: CardThemeData(
-        color: Colors.grey[900],
-        elevation: 0,
-      ),
+      cardTheme: CardThemeData(color: Colors.grey[900], elevation: 0),
 
       textTheme: TextTheme(
         headlineLarge: TextStyle(
@@ -37,14 +34,8 @@ class AppTheme {
           fontWeight: FontWeight.bold,
           color: goldPrimary,
         ),
-        bodyLarge: TextStyle(
-          fontSize: 16,
-          color: Colors.white,
-        ),
-        bodyMedium: TextStyle(
-          fontSize: 14,
-          color: Colors.white,
-        ),
+        bodyLarge: TextStyle(fontSize: 16, color: Colors.white),
+        bodyMedium: TextStyle(fontSize: 14, color: Colors.white),
       ),
 
       elevatedButtonTheme: ElevatedButtonThemeData(
@@ -84,61 +75,6 @@ class AppTheme {
           TargetPlatform.macOS: CupertinoPageTransitionsBuilder(),
           TargetPlatform.windows: FadeUpwardsPageTransitionsBuilder(),
         },
-      ),
-    );
-  }
-
-  // Keep light theme for future (if you want a light mode toggle)
-  static ThemeData get lightTheme {
-    return ThemeData(
-      useMaterial3: true,
-
-      colorScheme: ColorScheme.light(
-        primary: goldPrimary,
-        secondary: darkBlue,
-        tertiary: lightGold,
-        surface: Colors.white,
-      ),
-
-      textTheme: TextTheme(
-        headlineLarge: TextStyle(
-          fontSize: 32,
-          fontWeight: FontWeight.bold,
-          color: goldPrimary,
-        ),
-        bodyLarge: TextStyle(
-          fontSize: 16,
-          color: goldPrimary,
-        ),
-      ),
-
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-          backgroundColor: goldPrimary,
-          foregroundColor: Colors.white,
-          padding: EdgeInsets.symmetric(vertical: 16),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
-          ),
-        ),
-      ),
-
-      inputDecorationTheme: InputDecorationTheme(
-        filled: true,
-        fillColor: Colors.grey[900],
-
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12),
-          borderSide: BorderSide(color: Colors.grey[700]!, width: 1),
-        ),
-
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12),
-          borderSide: BorderSide(color: goldPrimary, width: 2),
-        ),
-
-        labelStyle: TextStyle(color: Colors.grey[400]),
-        hintStyle: TextStyle(color: Colors.grey[600]),
       ),
     );
   }
